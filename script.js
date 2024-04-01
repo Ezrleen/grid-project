d6a0e33378190ce25098cc49a29d9f31d50844db
Ezrleen <boughanmiaziz404@gmail.com>
2024-04-01 14:25:41 +0100

    event on div + function Click_left_toChange

--- script.js
+++ script.js
@@ -29,24 +29,43 @@ container1.style.marginBottom = "15px";
 
 container.style.border = "1px yellow solid";
 container.style.display = "flex";
-container.style.width = "600px";
-container.style.height = "600px";
+container.style.width = "800px";
+container.style.height = "800px";
 container.style.flexWrap = "wrap";
 container.style.backgroundColor = "gray";
 
 container.style.marginLeft = "auto";
 container.style.marginRight = "auto";
+ let isDrawing = false;
 function setgrid(){
     const size_grid = input.value;
     container.innerHTML = "";
     for (let i = 0; i < size_grid; i++) {
         for (let j = 0; j < size_grid; j++) {
             const newDiv = document.createElement('div');   
-            newDiv.style.width = `calc(600px / ${size_grid} - 2px )`;
-            newDiv.style.height = `calc(600px / ${size_grid} - 2px)`;
-            newDiv.style.backgroundColor = "white"; // Set background color
-            newDiv.style.border = "1px solid blue"; // Set border
+            newDiv.style.width = `calc(800px / ${size_grid} - 2px )`;
+            newDiv.style.height = `calc(800px / ${size_grid} - 2px)`;
+            newDiv.style.backgroundColor = "white";
+            newDiv.style.border = "1px solid blue";
+            newDiv.addEventListener('mouseover',test)//*drawing only when mouse moving + leftbutton clicked)*/
+
+
             container.appendChild(newDiv);
     }
+    
 }}
+ 
+function test(e) {
+    
+    container.addEventListener('mousedown', () => { //* test if the left mouse button is clicked in the grid)/*
+        isDrawing = true;
+    });
+    container.addEventListener('mouseup', () => { //* test if the left mouse button is  not clicked in the grid)/*
+        isDrawing = false;
+    });
+    if (isDrawing) {
+        console.log(e.target)
+        e.target.style.backgroundColor = "red"; //* if clicked the e.target which is the div is changing color)*/
 
+    }
+}
